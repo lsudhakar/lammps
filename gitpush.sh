580d70069370a7d60cc0724e29f5002f72d20c24
Sridhar Kannam <sridhar@oc5713633217.ibm.com>
2015-05-26 11:59:23 +1000

    update

--- gitpush.sh
+++ gitpush.sh
@@ -2,5 +2,6 @@ git pull
 git init
 git add *
 git commit -m "update"
-git remote add origin https://github.com/sridharkumarkannam/lammps.git
+#git remote add origin https://github.com/sridharkumarkannam/lammps.git
+git remote add origin git@github.com:sridharkumarkannam/lammps.git
 git push -u origin master
